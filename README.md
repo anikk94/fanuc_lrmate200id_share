d69c0721cd0add83f96605f0c4522e13bd06abed
Anirudh Krishnan Komaralingam <anirudhkrishnanisavailable@gmail.com>
2024-01-12 06:15:45 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# fanuc_lrmate200id_share
+Runs rviz2 with fanuc_lrmate200id
+
+# Instructions
+```
+colcon build --packages-select fanuc_lrmate200id_share
+source install/setup.bash
+ros2 launch fanuc_lrmate200id_share rviz.launch.py
+```
---
+++ launch/rviz.launch.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command
+from launch.substitutions import FindExecutable
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]),
+        " ",
+        PathJoinSubstitution([FindPackageShare("fanuc_lrmate200id_share"), "urdf", "fanuc_only.xacro"]),
+    ])
+    robot_description = {'robot_description': robot_description_content}
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[
+            robot_description,
+            {'use_sim_time': True},
+        ]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+        # parameters=[{'use_sim_time': True}]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', PathJoinSubstitution([FindPackageShare("fanuc_lrmate200id_share"), "config", "fanuc.rviz"])],
+        parameters=[robot_description]
+    )
+
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(rviz_node)
+
+    return ld
